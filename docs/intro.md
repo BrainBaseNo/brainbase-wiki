fbd73626787bfc18f7ea2e62d036593c9a6d75ea
Ketil Jensen <ketilj@gmail.com>
2022-07-12 17:06:31 +0200

    initial commit

---
+++ docs/intro.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+---
+
+# BrainBase Wiki
+
+BrainBase wiki er en samling av ressurser som er nyttige for alle som driver som selvstendige konsulenter eller små selskaper. 
+
+## Hvordan bidra
+
+Alle kan bidra med innhold på wikien. 
---
+++ docusaurus.config.js
@@ -0,0 +1,132 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'BrainBase Wiki',
+  tagline: 'Det foretrukne nettverket for selvstendige konsulenter',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'BrainBase', // Usually your GitHub org/user name.
+  projectName: 'brainbase-wiki', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
+  i18n: {
+    defaultLocale: 'no',
+    locales: ['no'],
+  },
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'BrainBase Wiki',
+        logo: {
+          alt: 'BrainBase AS',
+          src: 'img/brain-green-64.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Wiki',
+          },
+          {href: 'https://blogg.brainbase.no', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/BrainBaseNo/brainbase-web',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Wiki',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Slack',
+                href: 'https://brainbaseteam.slack.com'
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/company/brainbase',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/brainbaseno',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                href: 'https://blogg.brainbase.no',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/brainbaseno/brainbase-wiki',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
